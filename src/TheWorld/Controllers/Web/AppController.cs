bfe82d7d6dff2fe9c3d40cb220ebf78be825159a
Adam Smith <asmith@uxcps.local>
2017-03-10 17:10:24 +1100

    First commit

---
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheWorld.Services;
+using TheWorld.ViewModels;
+
+namespace TheWorld.Controllers.Web
+{
+    public class AppController : Controller 
+    {
+        private IMailServices _mailService;
+        private IConfigurationRoot _config;
+
+        public AppController(IMailServices mailService, IConfigurationRoot config)
+        {
+            _mailService = mailService;
+            _config = config;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (model.Email.Contains("fake.com"))
+            {
+                ModelState.AddModelError("", "We do not support Fake.com addresses");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _mailService.Sendmail(_config["MailSettings:ToAddress"], model.Email, "From The World", model.Message);
+                ViewBag.UserMessage = "Message Sent";
+                ModelState.Clear();
+            }
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,34 @@
+﻿// site.js
+(function () {
+
+    var $sidebarAndWrapper = $("#sidebar, #wrapper");
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
+    });
+
+
+
+    //var ele = $("#username");
+    //ele.text("Dylan Smith");
+
+    //var main = $("#main");
+    //main.on("mouseenter", function () {
+    //    main.style.backgroundColor = "#888";
+    //});
+    //main.on("mouseleave", function () {
+    //    main.style.backgroundColor = "";
+    //});
+
+    //var munItem = $("ul.menu li a");
+    //munItem.on("click", function () {
+    //    var me = $(this);
+    //    alert(me.text());
+    //});
+    
+})();
+
